65f9475c2e803081a369cf6d8ac728ecc70051b4
Victoria Meng <nights1ststar@protonmail.com>
2018-07-22 22:05:58 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,4 +15,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
     redirect_to @recipe
   end
+
+  private 
+
+  def recipe_params 
+    params.require(:recipe).permit(:title)
+  end
 end
